32d31bf7e0efd01db55df4cee78c5b53fa60e730
Ygor Santos <santosygor2007@gmail.com>
2026-07-27 20:45:01 -0300

    feat: add register and login validation fields

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,28 +5,66 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export async function register(name, email, password) {
-    const alreadyExists = await user.findOne({email});
-    if(alreadyExists) throw new ConflictError("An account with this email already exists");
+
+    validateRegisterFields(name, email, password);
+
+    const alreadyExists = await user.findOne({ email });
+    if (alreadyExists) throw new ConflictError("An account with this email already exists");
 
     const hashPassword = await bcrypt.hash(password, 10);
 
-    const registeredUser = await user.create({name, email, hashPassword}); 
+    const registeredUser = await user.create({ name, email, hashPassword });
 
     return registeredUser;
 }
 
 export async function login(email, password) {
-    const userExists = await user.findOne({email});
-    if(!userExists) throw new BadRequestError("Invalid email or password");
+
+    validateLoginFields(email, password);
+
+    const userExists = await user.findOne({ email });
+    if (!userExists) throw new BadRequestError("Invalid email or password");
 
     const validationPassword = await bcrypt.compare(password, userExists.hashPassword);
-    if(!validationPassword) throw new BadRequestError("Invalid email or password");
+    if (!validationPassword) throw new BadRequestError("Invalid email or password");
 
-    const token = generateAccessToken({id: userExists._id});
+    const token = generateAccessToken({ id: userExists._id });
 
     return token;
 }
 
-function generateAccessToken(payload){
-    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"})
+// HELPERS FUNCTIONS
+function generateAccessToken(payload) {
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" })
+}
+
+function validateRegisterFields(name, email, password) {
+
+    const formattedEmail = email?.trim().toLowerCase();
+
+    if (!name?.trim() || !email?.trim() || !password?.trim())
+        throw new BadRequestError("The name, email and password are required");
+    if (!emailIsValid(formattedEmail))
+        throw new BadRequestError("Invalid email format");
+    if (password.length < 4 || password.length > 16)
+        throw new BadRequestError("The password must be between 4 and 16 characters long.");
+}
+
+function validateLoginFields(email, password) {
+
+    const formattedEmail = email?.trim().toLowerCase();
+
+    if (!email?.trim() || !password?.trim())
+        throw new BadRequestError("The email and password are required");
+    if (!emailIsValid(formattedEmail))
+        throw new BadRequestError("Invalid email format");
+    if (password.length < 4 || password.length > 16)
+        throw new BadRequestError("The password must be between 4 and 16 characters long.");
+}
+
+function emailIsValid(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailRegex.test(email);
+
+    return isValid;
 }
