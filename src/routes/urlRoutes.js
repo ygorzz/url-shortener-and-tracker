a9856e3ae30abe683e98e1caba2c7aeb9efbeb76
Ygor Santos <santosygor2007@gmail.com>
2026-07-29 20:34:01 -0300

    refactor: update routes parameters order

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -6,11 +6,11 @@ import auth from "../middlewares/auth.js";
 const routes = express.Router();
 
 routes
-    .post("/shortUrl", rateLimit.creationLimiter, auth, UrlController.createShortUrl)
+    .post("/shortUrls", rateLimit.creationLimiter, auth, UrlController.createShortUrl)
     .get("/shortUrls", rateLimit.getUrlsLimiter, auth, UrlController.findAllShortUrls)
-    .get("/stats/:shortUrl", rateLimit.getStatsLimiter, auth, UrlController.findShortUrlStats)
-    .get("/redirect/:shortUrl", rateLimit.redirectLimiter, auth, UrlController.redirectShortUrl)
-    .delete("/:shortUrl", rateLimit.deletionLimiter, auth, UrlController.deleteShortUrl)
-    .put("/renew/:shortUrl", rateLimit.updateLimiter, auth, UrlController.renewShortUrl)
+    .get("/shortUrls/:shortUrl/stats", rateLimit.getStatsLimiter, auth, UrlController.findShortUrlStats)
+    .get("/shortUrls/:shortUrl/redirect", rateLimit.redirectLimiter, auth, UrlController.redirectShortUrl)
+    .put("/shortUrls/:shortUrl/renew", rateLimit.updateLimiter, auth, UrlController.renewShortUrl)
+    .delete("/shortUrls/:shortUrl", rateLimit.deletionLimiter, auth, UrlController.deleteShortUrl)
 
 export default routes;
