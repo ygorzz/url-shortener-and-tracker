babe86e06c0e0eb201520b0c41c80ca1d89bfc66
Ygor Santos <santosygor2007@gmail.com>
2026-08-07 20:32:29 -0300

    feat: add env variables validations

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 
-export default function dbConnection(){
+export default function dbConnection() {
+    if (!process.env.DB_CONNECTION_STRING) {
+        throw new Error("DB_CONNECTION_STRING is required");
+    }
+
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is required");
+    }
     mongoose.connect(process.env.DB_CONNECTION_STRING)
     return mongoose.connection;
 }
